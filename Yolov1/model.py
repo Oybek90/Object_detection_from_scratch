862d50fa93aa9c4034a766bdea7577b2a7ca6105
Oybek Eraliev <67625492+Oybek90@users.noreply.github.com>
2023-08-29 21:03:02 +0900

    new

---
+++ Yolov1/model.py
@@ -0,0 +1,49 @@
+import torch
+import torch.nn as nn
+
+architecture_config = [
+    # Tuple: (kernel_size, number_of_filter, stride, padding)
+    (7, 64, 2, 3),
+    "M",
+    (3, 192, 1, 1),
+    "M",
+    (1, 128, 1, 0),
+    (3, 256, 1, 1),
+    (1, 256, 1, 0),
+    (3, 512, 1, 1),
+    "M",
+    # List: tuples and then last integer represents number of repeats
+    [(1, 256, 1, 0), (3, 512, 1, 1), 4],
+    (1, 512, 1, 0),
+    (3, 1024, 1, 1),
+    "M",
+    [(1, 512, 1, 0), (3, 1024, 1, 1), 2],
+    (3, 1024, 1, 1),
+    (3, 1024, 2, 1),
+    (3, 1024, 1, 1),
+    (3, 1024, 1, 1),
+]
+
+class CNNBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, **kwargs):
+        super(CNNBlock, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, bias=False, **kwargs)
+        self.batchnorm = nn.BatchNorm2d(out_channels)
+        self.leakyrelu = nn.LeakyReLU(0.1)
+
+    def forward(self, x):
+        return self.leakyrelu(self.batchnorm(self.conv(x)))
+    
+class Yolov1(nn.Module):
+    def __init__(self, in_channels = 3, **kwargs):
+        super(Yolov1).__init__()
+        self.architecture = architecture_config
+        self.in_channels = in_channels
+        self.darsknet = self._create_conv_layers(self.architecture)
+        self.fcs = self._create_fcs(**kwargs)
+
+    def forward(self, x):
+        x = self.darsknet(x)
+        return self.fcs(torch.flatten(x, start_dim=1))
+    
+    
