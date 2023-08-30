8ae39d9eba8788934b511908f681ac65d275e48f
Oybek Eraliev <67625492+Oybek90@users.noreply.github.com>
2023-08-30 17:08:29 +0900

    new

--- Yolov1/model.py
+++ Yolov1/model.py
@@ -46,4 +46,54 @@ class Yolov1(nn.Module):
         x = self.darsknet(x)
         return self.fcs(torch.flatten(x, start_dim=1))
     
-    
+    def __create_conv_layers(self, architecture):
+        layers = []
+        in_channels = self.in_channels
+
+        for x in architecture:
+            if type(x) == tuple:
+                layers += [CNNBlock(
+                    in_channels, x[1], kernel_size=x[0], stride=x[2], padding=x[3],
+                )
+            ]
+                in_channels = x[1]
+            elif type(x) == str:
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            elif type(x) == list:
+                conv1 = x[0]
+                conv2 = x[1]
+                num_repeats = x[2]
+
+                for _ in range(num_repeats):
+                    layers += [
+                        CNNBlock(
+                        in_channels,
+                        conv1[1],
+                        kernel_size = conv1[0],
+                        stride = conv1[2],
+                        padding = conv1[3],
+                        )
+                    ]
+                    layers += [
+                        CNNBlock(
+                        conv1[1],
+                        conv2[1],
+                        kernel_size = conv2[0],
+                        stride = conv2[2],
+                        padding = conv2[3],
+                        )
+                    ]
+
+                    in_channels = conv2[1]
+        return nn.Sequential(*layers)
+    def _create_fcs(self, split_size, num_boxes, num_classes):
+        S, B, C = split_size, num_boxes, num_classes
+        return nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(1024*S*S, 496), #Original paper this hsould be 4096
+            nn.Dropout(0.0),
+            nn.LeakyReLU(0.1),
+            nn.Linear(496, S*S*(C+B*5)), #(S, S, 30) where C+B*5 = 30
+        )
+
+
